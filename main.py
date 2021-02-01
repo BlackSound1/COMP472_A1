34b13ff733c5cf23265a28a53f16fd34ecf1e112
EvanMateo7 <gevanmateo@gmail.com>
2021-02-01 13:48:55 -0500

    Add task 0 and 1

---
+++ main.py
@@ -0,0 +1,28 @@
+import utils
+import matplotlib.pyplot as plt
+
+"""Task 0 - Split training and evaluation data"""
+
+# Read data
+all_docs, all_labels = utils.read_documents('all_sentiment_shuffled.txt')
+
+# Split point between training and evaluation
+split_point = int(0.80*len(all_docs))
+
+# Get training and evaluation data
+train_docs = all_docs[:split_point]
+train_labels = all_labels[:split_point]
+eval_docs = all_docs[split_point:]
+eval_labels = all_labels[split_point:]
+
+"""Task 1 - Plot label distribution"""
+# Get label distribution
+label_distribution = utils.get_label_distribution(all_labels)
+print(label_distribution)
+
+# Plot label distribution
+plt.figure()
+plt.bar(label_distribution.keys(), label_distribution.values(), 0.8)
+for x, y in enumerate(label_distribution.values()):
+    plt.text(x, y, str(y), horizontalalignment='center')
+plt.show()
---
+++ utils.py
@@ -0,0 +1,32 @@
+
+def read_documents(path: str) -> (list, list):
+    """Reads data file.
+
+    Args:
+      path (str): Path to data file.
+
+    Returns:
+      Tuple of lists (words, labels) 
+    """
+    labels, docs = [], []
+    with open(path, 'rb') as f:
+        for line in f:
+            words = line.strip().split()
+            labels.append(words[0])
+            docs.append(words[3:])
+    return docs, labels
+
+def get_label_distribution(all_labels: list) -> dict:
+    """Calculates distribution of labels.
+
+    Args:
+      all_labels (list): List of all labels.
+
+    Returns:
+      Dictionary keys (labels) and values (count)
+
+      e.g. { 'health': 3, 'books': 45 }
+    """
+    labels = sorted(list(set(all_labels)))
+    label_counts = {label: all_labels.count(label) for label in labels}
+    return label_counts
