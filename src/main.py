7e5874f21deeb1f6aad9b57a2287749611c0add8
EvanMateo7 <gevanmateo@gmail.com>
2021-02-07 13:30:55 -0500

    Add naive bayes model

--- src/main.py
+++ src/main.py
@@ -1,5 +1,10 @@
 import utils
+import numpy as np
 import matplotlib.pyplot as plt
+from sklearn.naive_bayes import BernoulliNB
+from sklearn import metrics
+from sklearn.feature_extraction.text import CountVectorizer
+
 
 """Task 0 - Split training and evaluation data"""
 
@@ -9,19 +14,30 @@ all_docs, all_labels = utils.read_documents('input/all_sentiment_shuffled.txt')
 split_point = int(0.80*len(all_docs))
 
 # Get training and evaluation data
-train_docs = all_docs[:split_point]
+count_vect = CountVectorizer(tokenizer=lambda txt: txt.split())
+all_docs_as_strings = list(' '.join(doc) for doc in all_docs)
+all_docs_vect = count_vect.fit_transform(all_docs_as_strings)
+
+train_docs = all_docs_vect[:split_point]
 train_labels = all_labels[:split_point]
-eval_docs = all_docs[split_point:]
+eval_docs = all_docs_vect[split_point:]
 eval_labels = all_labels[split_point:]
 
 """Task 1 - Plot label distribution"""
-# Get label distribution
-label_distribution = utils.get_label_distribution(all_labels)
-print(label_distribution)
+# # Get label distribution
+# label_distribution = utils.get_label_distribution(all_labels)
+# print(label_distribution)
 
-# Plot label distribution
-plt.figure()
-plt.bar(label_distribution.keys(), label_distribution.values(), 0.8)
-for x, y in enumerate(label_distribution.values()):
-    plt.text(x, y, str(y), horizontalalignment='center')
-plt.show()
+# # Plot label distribution
+# plt.figure()
+# plt.bar(label_distribution.keys(), label_distribution.values(), 0.8)
+# for x, y in enumerate(label_distribution.values()):
+#     plt.text(x, y, str(y), horizontalalignment='center')
+# plt.show()
+
+"""Task 2 - Naive Bayes"""
+multiNB = BernoulliNB()
+multiNB.fit(train_docs, train_labels)
+
+y_pred = multiNB.predict(eval_docs)
+print(metrics.accuracy_score(eval_labels, y_pred))
